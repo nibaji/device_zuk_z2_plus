71e2a3c5dde87d054be8512c90ae1cd35c3adb57
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    Moved Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f
    
    	modified:   device.mk
    	new file:   overlay-lineage/frameworks/base/core/res/res/values/config.xml
    	new file:   overlay-lineage/packages/apps/CellBroadcastReceiver/res/values/config.xml
    	new file:   overlay-lineage/vendor/cmsdk/cm/res/res/values/config.xml
    	modified:   overlay/packages/apps/CellBroadcastReceiver/res/values/config.xml

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Display
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/vendor/etc/permissions/android.hardware.opengles.aep.xml \
