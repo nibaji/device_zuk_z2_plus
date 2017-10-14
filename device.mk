2de58d10e73f51149bd4e3522eb409171ee8d045
dd3boh <dade.garberi@gmail.com>
2017-10-14 18:03:57 +0200

    z2_plus: wifi: Add back WCNSS_cfg.dat
    * From LA.UM.6.5.r1-03400-8x96.0
    
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- device.mk
+++ device.mk
@@ -174,7 +174,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/vendor/etc/permissions/android.hardware.wifi.direct.xml \
