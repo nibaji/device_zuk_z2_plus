c79b7ae94d8e9a73711812d2eab1bc6869a55c3c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-12 19:29:06 -0200

    rootdir: Introduce 'SafailNet'
    SafailNet is inspired on magisk hide. (Thanks to topjohnwu)
    
    https://github.com/topjohnwu/Magisk/blob/master/jni/magiskhide/magiskhide.c
    
    The purpose is bypass safetynet without installing any zip via twrp
    
    And these commits too:
    
    https://github.com/PixelExperience/kernel_motorola_msm8953/commit/dc9c2f35e988123abf2aa50d30b8b324bc377fbf
    https://github.com/PixelExperience/kernel_motorola_msm8953/commit/ec5b1bb6821098ebb30a37cbf5d2c52286cacbfd
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -78,3 +78,12 @@ LOCAL_SRC_FILES		:= etc/init.zuk.usb.rc
 LOCAL_VENDOR_MODULE	:= true
 LOCAL_MODULE_RELATIVE_PATH	:= init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.safailnet.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.safailnet.rc
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_RELATIVE_PATH	:= init/hw
+include $(BUILD_PREBUILT)
