d50620c2a88fafe6d8ff808cade033250dcae18b
Davide Garberi <dade.garberi@gmail.com>
2017-11-25 14:17:40 +0100

    z2_plus : Add calib.cfg from ZUI
    * This is for the assertive display
    * The configuration was already there in the system.prop without the file though
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- device.mk
+++ device.mk
@@ -273,6 +273,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/vendor/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/calib.cfg:system/etc/calib.cfg
+
 # Power
 PRODUCT_PACKAGES += \
     power.msm8996
