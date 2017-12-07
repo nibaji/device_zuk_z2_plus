d4fb83b28893f81406e5311526151a136306f631
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    z2_plus: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -70,6 +70,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
