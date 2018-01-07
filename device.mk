0020e4609aff656da87ec1311ed0fb637e019656
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 14:01:18 -0800

    z2_plus: Add QTI privileged app permissions
    Change-Id: Ide1b79d83624e8aba2ee17979efacc356dfacb55
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # MSM IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
