95187fc98ad23f1148293c8e071c286e90f73aab
Nidhun Balaji T.R <nidhunbalaji@gmail.com>
2018-01-23 00:04:21 +0530

    Added : Basic Make files 	new file:   Android.mk 	new file:   AndroidProducts.mk 	new file:   BoardConfig.mk 	new file:   device.mk 	new file:   full_z2_plus.mk 	new file:   lineage.mk

---
+++ lineage.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/zuk/z2_plus/full_z2_plus.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := lineage_z2_plus
+BOARD_VENDOR := zuk
+PRODUCT_DEVICE := z2_plus
+PRODUCT_GMS_CLIENTID_BASE := android-zuk
+TARGET_VENDOR_PRODUCT_NAME := z2_plus
+TARGET_VENDOR_DEVICE_NAME := z2_plus
+
+# Overlays (inherit after vendor/lineage to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="z2_plus" \
+    PRODUCT_NAME="z2_plus" \
+    BUILD_FINGERPRINT="ZUK/z2_plus/z2_plus:7.0/NRD90M/3.1.117_171110:user/release-keys" \
+    PRIVATE_BUILD_DESC="z2_plus-user 7.0 NRD90M 3.1.117_171110 release-keys"
+
