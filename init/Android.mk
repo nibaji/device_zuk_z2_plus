ec2ebf7ecb02f654c483612f9497155daae70ff1
Nidhun Balaji T.R <nidhunbalaji@gmail.com>
2018-02-14 00:25:04 +0530

    system props -update

--- init/Android.mk
+++ init/Android.mk
@@ -23,7 +23,7 @@ LOCAL_C_INCLUDES := \
     system/core/base/include \
     system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_msm8996.cpp
-LOCAL_MODULE := libinit_msm8996
+LOCAL_SRC_FILES := init_z2_plus.cpp
+LOCAL_MODULE := libinit_z2_plus
 
 include $(BUILD_STATIC_LIBRARY)
